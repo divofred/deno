ad770068b1e8dbca871e8110b4b60f1e98330ceb
divofred <fredrickemmanuelc@gmail.com>
2023-03-07 14:06:57 +0100

    first commit

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,7 @@
 import { MongoClient } from 'https://deno.land/x/mongo/mod.ts';
-import { load } from 'https://deno.land/x/tiny_env/mod.ts';
 
-load({
-  dotEnvPath: './.env',
-});
 const client = new MongoClient();
-const MONGO_URI = Deno.env.get('MONGO_URI') + '=SCRAM-SHA-1';
+const MONGO_URI = Deno.env.get('MONGO_URI');
 
 await client.connect(MONGO_URI);
 const db = client.database('deno');
